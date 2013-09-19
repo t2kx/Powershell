dbf0810d34a1f9e6c27f4a4133a17b91ba9f4d98
techmike2kx <mike@azug.be>
2013-09-19 12:06:46 +0200

    rr
    rr

---
+++ 2.ps1
@@ -0,0 +1,1 @@
+﻿New-AzureQuickVM -Name CB-QVM-001 -ServiceName CB-QVM-001 -Location "West Europe" -InstanceSize ExtraSmall -AdminUsername ikke -Password "CloudBur$t2013" -ImageName "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-Datacenter-201307.01-en.us-127GB.vhd" -Windows
